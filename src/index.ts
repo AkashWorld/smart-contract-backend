70b8decf54cc73219b852f93337740f7aba4b6b3
Rizwan Chowdhury <rizwanchowdhury111@gmail.com>
2019-10-05 13:54:16 -0400

    Created router file, updated index.ts and created main.ts file

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,21 @@
+import express,{Request,Response,Next} from "express";
+import {router} from "./router"
+
 export function helloWorld() {
 	return 'Hello, ' + 'World';
 }
 
-async function main() {
-	console.log('Set up express here');
-}
+const app = express();
+	/* *Routes will be used to navigate through webapp, based on the url entry at the endpoint
+		 *LogIn is for the login page and its operations
+		 *EntryForm is the page where users will enter their data and a form will accept them
+	*/
+app.use('/',router);
+app.use('/LogIn',router);
+app.use('/EntryForm',router);
 
-main();
+
+
+
+//export this module to be used in making
+export default app;
