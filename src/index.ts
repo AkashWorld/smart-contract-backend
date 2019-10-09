949762be8a527bd9572d318cb5587c5e70f2e9d2
Rizwan Chowdhury <rizwanchowdhury111@gmail.com>
2019-10-08 22:10:43 -0400

    third time

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import express,{Request,Response,Next} from "express";
-import {router} from "../router"
+import {router} from "./router/router"
 
 export function helloWorld() {
 	return 'Hello, ' + 'World';
 }
 
-const app = express();
+export const app = express();
 	/* *Routes will be used to navigate through webapp, based on the url entry at the endpoint
 		 *LogIn is for the login page and its operations
 		 *EntryForm is the page where users will enter their data and a form will accept them
@@ -13,9 +13,3 @@ const app = express();
 app.use('/',router);
 app.use('/LogIn',router);
 app.use('/EntryForm',router);
-
-
-
-
-//export this module to be used in making
-export default app;
