ca8288ab6509138c56f291b27336b5f89467adc8
Rizwan Chowdhury <rizwanchowdhury111@gmail.com>
2019-10-09 19:10:38 -0400

    changes

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,21 +1,19 @@
-
-export var router : Router;
+import { Request, Response, Router, Next } from 'express';
+export let router: Router = Router();
 
 //for now just making the paths so that when the different pages set up we can fill in
 
 /* *The routes will change the webpage based on the input url ending
-   * Any functions or tasks that will be required when loading a page will go where
-   * For ex: if loading a graph page, function to load the data will go where
-*/
+ * Any functions or tasks that will be required when loading a page will go where
+ * For ex: if loading a graph page, function to load the data will go where
+ */
 
-router.get('/',function(req:Request,res:Response,next:Next){
+router.get('/', function(req: Request, res: Response, next: Next) {});
 
-});
-
-router.get('/LogIn',function(req:Request,res:Response,next:Next){
+router.get('/LogIn', function(req: Request,res: Response,next: Next) {
 
 });
 
-router.get('/EntryForm',function(req:Request,res:Response,next:Next){
+router.get('/EntryForm', function(req: Request,res: Response,next: Next) {
 
 });
