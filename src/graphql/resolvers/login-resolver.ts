7bdaf9874620bdd1429cd51eef0a47c844e46af8
Rizwan Chowdhury <rizwanchowdhury111@gmail.com>
2019-10-27 12:30:18 -0400

    extra comments and stuff

--- src/graphql/resolvers/login-resolver.ts
+++ src/graphql/resolvers/login-resolver.ts
@@ -1,17 +1,16 @@
-import {loginDataInput} from '../../services/login/loginpost'
-import * as express from 'express';
-import {Router,Response,Request,NextFunction} from 'express';
-import * as bodyparser from 'body-parser';
-import * as httpserver from 'http';
-//import Web3 from 'web3';
-const Web3 = require('web3');
+/*  Resolver obtains the unsigned_address from the query request
+    then it calls the loginDataInput to obtained the singed_address
+    which then is returned as query.
 
-var web3 = new Web3(new Web3.providers.HttpProvider("HTTP://127.0.0.1:7545"));
+*/
+
+
+import {loginDataInput} from '../../services/login/loginpost'
+
 
 const resolver = {
   Query: {
     login:(_:any,args:{unsigned_address:string},context:any) => {
-      //var signedAddress = web3.eth.sign("Authorization",args.unsigned_address).then()
       var signedAddress = loginDataInput(args.unsigned_address);
       return{
         signed_address: signedAddress
