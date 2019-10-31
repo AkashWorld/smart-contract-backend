c709bf5e9398fdf9fa04bec5e690bf50e1d02843
Rizwan Chowdhury <rizwanchowdhury111@gmail.com>
2019-10-31 00:45:13 -0400

    fix up login resolver

--- src/graphql/resolvers/index.ts
+++ src/graphql/resolvers/index.ts
@@ -2,7 +2,6 @@ import _ from 'lodash';
 import helloWorldResolver from './hello-world-resolver';
 import loginResolver from './login-resolver';
 import userDescriptorResolver from './user-descriptor-resolvers';
-import loginResolver from './login-resolver';
 
 export const resolvers = _.merge(
 	{},
