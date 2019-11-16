fca0780961c16c1e9ed20933ab2d08696dc385e1
SuvaShahria <suvz@PC-2.localdomain>
2019-11-15 20:14:17 -0500

    update

--- src/services/global-descriptor-service.ts
+++ src/services/global-descriptor-service.ts
@@ -1,6 +1,3 @@
-/**
- * Khalid Akash 2019
- */
 import BN from 'bn.js';
 import fs from 'fs';
 import Web3 from 'web3';
@@ -11,42 +8,21 @@ import loadContractAddress from '../utilities/contract-address-loader';
 import PromiEvent from 'web3/promiEvent';
 import { TransactionReceipt } from 'web3/types';
 
-/**
- * This class's purpose is to be an abstraction for interacting with the Blockchain, and in particular,
- * the GlobalDescriptor Smart Contract. This class implements methods that are in the smart contract and executes
- * it on behalf of the invoker of this class. The actual smart contract can be found in ./contracts/GlobalDescriptor.sol
- *
- * We want to create a class like this for every smart contract.
- */
-export class UserDescriptorService {
-	/**
-	 * USER_DESCRIPTOR_ABI_PATH is the path to the JSON that has information about the SmartContract that
-	 * web3js can read. This allows Web3JS to add methods to a contract object that we can invoke from JavaScript.
-	 */
-	private static readonly USER_DESCRIPTOR_ABI_PATH =
+export class GlobalDescriptorService {
+
+	private static readonly GLOBAL_DESCRIPTOR_ABI_PATH =
 		'build/contracts/GlobalDescriptor.json';
 
 	private static BNToNumber(val: BN): number {
 		return parseFloat((val as unknown) as string);
 	}
-	/**
-	 * Web3 is the main JavaScript client that allows us to interact with nodes in the blockchain.
-	 * It is what Truffle uses in the backend, but we can invoke it directly to get more control and
-	 * not rely too much on the Truffle framework.
-	 * https://web3js.readthedocs.io/en/v1.2.0/getting-started.html
-	 */
+
 	private readonly web3Client: Web3;
-	/**
-	 * Local Smart Contract object that we can invoke methods on which will automatically invoke methods on the
-	 * actual Smart Contract in the blockchain.
-	 */
+
 	private readonly contract: GlobalDescriptor;
 
 	private readonly DECIMAL_OFFSET: number = Math.pow(10, 4);
 
-	/**
-	 * Initializes the Web3 client that connects to the blockchain and creates the contract object.
-	 */
 	constructor(
 		web3Client = new Web3('http://localhost:7545'),
 		contract?: GlobalDescriptor
@@ -97,109 +73,7 @@ export class UserDescriptorService {
 		>;
 	}
 
-	/**
-	 * Abstraction for GlobalDescriptor (smart contract) method, getLatestValueForUnit(unit: string): number
-	 * See ./contracts/GlobalDescriptor.sol for the actual contract method
-	 * @param accountId ID of the account sending the request (Local blockchain autogenerates 10 accounts to use)
-	 * @param unit A unit such as lb, cm, miles, kilometer, etc
-	 * @param gas Optional paramter, defaults to 5,000,000. Need gas to perform any sort of operation.
-	 */
-	public async getLatestValueForUnit(
-		accountId: string,
-		unit: string,
-		gas = 5_000_000
-	): Promise<number> {
-		const txOptions: Tx = {
-			from: accountId,
-			gas
-		};
-		const value = ((await this.contract.methods
-			.getLatestUnitValue(unit)
-			.call(txOptions)) as unknown) as string;
-
-		return parseInt(value, 10) / this.DECIMAL_OFFSET;
-	}
-
-	/**
-	 * Abstraction for GlobalDescriptor (smart contract) method, getAllValuesRecordedForUnit(unit: string): number[]
-	 * See ./contracts/GlobalDescriptor.sol for the actual contract method
-	 * @param accountId ID of the account sending the request (Local blockchain autogenerates 10 accounts to use)
-	 * @param unit A unit such as lb, cm, miles, kilometer, etc
-	 * @param gas Optional paramter, defaults to 5,000,000. Need gas to perform any sort of operation.
-	 */
-	public async getAllValuesRecordedForUnit(
-		accountId: string,
-		unit: string,
-		gas = 5_000_000
-	): Promise<IDescriptor[]> {
-		const txOptions: Tx = {
-			from: accountId,
-			gas
-		};
-		const stringArray = await this.contract.methods
-			.getAllUnitValues(unit)
-			.call(txOptions);
-		return stringArray.map(val => {
-			return {
-				unit,
-				value:
-					UserDescriptorService.BNToNumber(val.unitValue) /
-					this.DECIMAL_OFFSET,
-				longitude:
-					UserDescriptorService.BNToNumber(val.longitude) /
-					this.DECIMAL_OFFSET,
-				latitude:
-					UserDescriptorService.BNToNumber(val.latitude) /
-					this.DECIMAL_OFFSET,
-				unixTimestamp: UserDescriptorService.BNToNumber(val.time)
-			};
-		});
-	}
-
-	public async getPaginatedValuesRecordedForUnit(
-		accountId: string,
-		unit: string,
-		start = 0,
-		count = 50,
-		gas = 5_000_000
-	): Promise<IDescriptor[]> {
-		const stringArray = await this.contract.methods
-			.getPaginatedUnitValues(unit, start, count)
-			.call({ from: accountId, gas });
-		return stringArray.map(val => {
-			return {
-				unit,
-				value:
-					UserDescriptorService.BNToNumber(val.unitValue) /
-					this.DECIMAL_OFFSET,
-				longitude:
-					UserDescriptorService.BNToNumber(val.longitude) /
-					this.DECIMAL_OFFSET,
-				latitude:
-					UserDescriptorService.BNToNumber(val.latitude) /
-					this.DECIMAL_OFFSET,
-				unixTimestamp: UserDescriptorService.BNToNumber(val.time)
-			};
-		});
-	}
-
-	/**
-	 * Abstraction for GlobalDescriptor (smart contract) method, getAllAvailableUnitForUser(): string[]
-	 * See ./contracts/GlobalDescriptor.sol for the actual contract method
-	 * @param accountId ID of the account sending the request (Local blockchain autogenerates 10 accounts to use)
-	 * @param gas Optional paramter, defaults to 5,000,000. Need gas to perform any sort of operation.
-	 */
-	public async getAllAvailableUnitsForUser(
-		accountId: string,
-		gas = 5_000_000
-	) {
-		const txOptions: Tx = {
-			from: accountId,
-			gas
-		};
-		return this.contract.methods.getAllAvailableUnits().call(txOptions);
-	}
-
+	
 	/**
 	 * Gets all the accounts in the blockchain and returns the ID of the account at
 	 * a certain index in the returned array.
@@ -223,7 +97,7 @@ export class UserDescriptorService {
 	 */
 	private getContractAbi(): any[] {
 		const rawJson = fs.readFileSync(
-			UserDescriptorService.USER_DESCRIPTOR_ABI_PATH
+			GlobalDescriptorService.GLOBAL_DESCRIPTOR_ABI_PATH
 		);
 		return JSON.parse(rawJson.toString()).abi;
 	}
